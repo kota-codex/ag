cd0a276107436469e16bfff0614106baa187d1da
Andrey Kalmatskiy <andrewkoder82@gmail.com>
2025-11-09 21:17:30 -0500

    remove incremental, it breaks prefix data with thunks, use link.exe for cinsistency with lld-link

--- out/bin/build-debug.bat
+++ out/bin/build-debug.bat
@@ -1,19 +1,16 @@
 @setlocal enabledelayedexpansion
 @if exist "%~dp0lld-link.exe" (
-   set linker="%~dp0lld-link.exe" /debug /out:
+   set linker="%~dp0lld-link.exe"
 ) else (
-   set linker="cl.exe" /Zi /Fe
+   set linker="link.exe"
 )
-@if exist "%~n1.obj" del "%~n1.obj"
-"%~dp0agc" -g -src "%~dp1." -src "%~dp0..\lib" -start "%~n1" -O0 -o "%~n1.obj" -L lnk.txt -D dep.txt
+"%~dp0agc" -g -src "%~dp1." -src "%~dp0..\lib" -start "%~n1" -O3 -o "%~n1" -L lnk.txt -D dep.txt
 @IF %ERRORLEVEL%==0 (
 	set /p objs=<lnk.txt
-	%linker%"%~n1.exe" !objs!
+	%linker% /OUT:"%~n1.exe" /DEBUG /INCREMENTAL:NO !objs!
 )
 @IF %ERRORLEVEL%==0 (
 	set /p "deps="<dep.txt
-	for %%f in (!deps!) do (
-		copy /D "%%~f" ".\" >nul
-	)
+	for %%f in (!deps!) do copy /D "%%~f" ".\" >nul
 )
 @endlocal
