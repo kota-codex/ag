1ea490161fcae42d8df93d1f3844f392d6767b21
Andrey Kalmatskiy <andrewkoder82@gmail.com>
2025-11-10 16:35:08 -0500

    log linker invocation params

--- out/bin/run-release.bat
+++ out/bin/run-release.bat
@@ -1,17 +1,18 @@
 @setlocal enabledelayedexpansion
+
 @if exist "%~dp0lld-link.exe" (
-   set linker="%~dp0lld-link.exe"
+    set linker="%~dp0lld-link.exe"
 ) else (
-   set linker="link.exe"
+    set linker="link.exe"
 )
+
 "%~dp0agc" -src "%~dp1." -src "%~dp0..\lib" -start "%~n1" -O3 -o "%~n1" -L lnk.txt -D dep.txt
-@IF %ERRORLEVEL%==0 (
-	set /p objs=<lnk.txt
-	%linker% /OUT:"%~n1.exe" /INCREMENTAL:NO !objs!
-)
-@IF %ERRORLEVEL%==0 (
-	set /p "deps="<dep.txt
-	for %%f in (!deps!) do copy /D "%%~f" ".\" >nul
-)
+@if errorlevel 1 exit /b %errorlevel%
+
+@set /p objs=<lnk.txt
+%linker% /OUT:"%~n1.exe" /INCREMENTAL:NO %objs%
+@if errorlevel 1 exit /b %errorlevel%
+
+@set /p "deps="<dep.txt
+@for %%f in (!deps!) do @copy /D "%%~f" ".\" >nul
 "%~n1.exe"
-@endlocal
