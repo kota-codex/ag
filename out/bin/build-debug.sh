608e42eec8466f18a2e27665d1ee62a7c9501b19
Andrey Kalmatskiy <andrewkoder82@gmail.com>
2025-11-14 16:44:32 -0500

    Update out/bin/run-dbg-release.sh

--- out/bin/build-debug.sh
+++ out/bin/build-debug.sh
@@ -6,14 +6,19 @@ SRC_DIR="$(dirname "$1")"
 OUT_NAME="$(basename "$1")"
 OUT_NAME="${OUT_NAME%.*}"  # remove ext
 
-"$AG_DIR/agc" -O3 -src "$SRC_DIR" \
+"$AG_DIR/agc" \
+    -g \
+    -src "$SRC_DIR" \
     -src "$AG_DIR/../lib::https://aglang.org/wp-json/repo/v1" \
-    -start "$OUT_NAME" -o "$OUT_NAME" -L lnk.txt -D dep.txt
+    -start "$OUT_NAME" \
+    -O0 \
+    -o "$OUT_NAME" \
+    -L lnk.txt \
+    -D dep.txt
 objs=$(<lnk.txt)
-gcc -no-pie -o "$OUT_NAME" $objs -lm
+gcc -no-pie -o "$OUT_NAME" $objs -g -lm
 
 deps=$(<dep.txt)
 for f in $deps; do
     cp -u "$f" .
 done
-"./$OUT_NAME"
